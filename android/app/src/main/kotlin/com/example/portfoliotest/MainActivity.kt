8da78500da569c6566ceab8c95c35502b48425e5
user <girithar@nearle.in>
2025-03-21 13:19:06 +0530

    Commit : 21-03-2025 1.Test check

--- android/app/src/main/kotlin/com/example/portfoliotest/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.portfoliotest
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:porfolio/screens/home_page.dart';
 
-void main() {
-  runApp(const MyApp());
+
+
+void main(){
+
+  runApp(MyApp());
 }
+
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: ThemeData(
+
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
     );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-
-      backgroundColor: Colors.orange,
-      appBar: AppBar(
-
-        backgroundColor: Colors.orange,
-
-        title: Text(widget.title),
-      ),
-      body: Container(
-        decoration: BoxDecoration(color: Colors.orangeAccent),
-
-      )
-    // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-
-// keytool -genkey -v -keystore D:\Test\portfolio-keystore.jks `
-// -storetype JKS -keyalg RSA -keysize 2048 -validity 10000 `
-// -alias portfolio
